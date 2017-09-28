bed2b015c6f2208f2ebc6ad27f349a6072f605b3
mohammed shabrez <31725739+mohshabrez@users.noreply.github.com>
2017-09-28 14:40:46 +0530

    Update mohprgm10.py

--- mohprgm10.py
+++ mohprgm10.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 n=int(input("Enter an integer:"))
+#the command enters the integer
 print("The divisors of the number are:")
+#guess it will display the divisors of the given integer by the for loop
 for i in range(1,n+1):
     if(n%i==0):
         print(i)
