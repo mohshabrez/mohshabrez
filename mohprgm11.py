78dc01e0ba81255d92604cb18b8d9dc8162014c4
mohammed shabrez <31725739+mohshabrez@users.noreply.github.com>
2017-09-28 14:56:38 +0530

    Update mohprgm11.py

--- mohprgm11.py
+++ mohprgm11.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 choice=["1","2","3"]
 	choice = input ("enter p to choice")
+	#here we enter our choice
 		cpu choice = choice[random.randint(1,3)]
+		# the command accepts the choice n plays with cpu
 		if choice==stone:
 			choice=paper
 			print("stone caugght paper",choice)
+			#this command decide whether who wins
 		else:
 			print("cpu wins")
 		if choice==paper:
